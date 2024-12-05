a524e085c62c8688fca8f2131d3ab9c938ac00fe
noerhdy <hidayatnoer11@gmail.com>
2024-12-06 06:53:02 +0700

    testing again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,jsx}",
@@ -10,7 +11,7 @@ module.exports = {
   prefix: "",
   theme: {
     container: {
-      center: "true",
+      center: true, // bisa hapus tanda kutip untuk value boolean
       padding: "2rem",
       screens: {
         "2xl": "1400px",
