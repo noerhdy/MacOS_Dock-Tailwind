2b15306a8ac2a60b6a7bfe0971776ff35a1eaa1d
noerhdy <hidayatnoer11@gmail.com>
2024-12-06 06:13:56 +0700

    Testing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
+    container: {
+      center: "true",
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
   },
+
   plugins: [],
 };
