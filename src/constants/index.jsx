cec0ffa8e76d0e8c14f416f7be33d579d5a42441
noerhdy <hidayatnoer11@gmail.com>
2024-12-06 06:28:19 +0700

    testing lg

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,38 +1,42 @@
 export const icons = [
   {
     name: "Behance",
-    url: "https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=hidayatnoer11@gmail.com",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/behance.png",
   },
   {
     name: "Dribbble",
-    url: "https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=hidayatnoer11@gmail.com",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/drible.png",
   },
   {
     name: "Threads",
-    url: "https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=hidayatnoer11@gmail.com",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/threads.png",
   },
   {
     name: "Instagram",
-    url: "https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=hidayatnoer11@gmail.com",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/ig.png",
   },
   {
     name: "Gmail",
-    url: "https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=hidayatnoer11@gmail.com",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/gm.webp",
   },
   {
     name: "Linkedin",
-    url: "https://www.linkedin.com/in/nur-hidayat21/",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/in.webp",
   },
   {
     name: "Github",
-    url: "https://github.com/noerhdy",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
     src: "/app/git.webp",
   },
-  { name: "X", url: "https://twitter.com/Noer1114", src: "/app/x.webp" },
+  {
+    name: "X",
+    url: "https://mac-os-dock-tailwind.vercel.app/",
+    src: "/app/x.webp",
+  },
 ];
